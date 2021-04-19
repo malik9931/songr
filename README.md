406f07ac8b7789da0c45031a5bd52520e114689a
Malek <engmalek.civil@gmail.com>
2021-04-19 18:25:13 +0300

    lab 12 is DONE

--- README.md
+++ README.md
@@ -32,3 +32,25 @@ Use the Spring Initializr to create a new application with artifact songr with W
          └── splashPage.html
     └── test
      └── SongrApplicationTests.java
+
+## Lab-12: Spring and REST 
+### Setup
+Increasing mor dependencies 
+```aidl
+dependencies {
+	compile 'org.springframework.boot:spring-boot-starter-data-jpa'
+	implementation('org.springframework.boot:spring-boot-starter-thymeleaf')
+	implementation('org.springframework.boot:spring-boot-starter-web')
+	runtimeOnly('org.postgresql:postgresql')
+	testImplementation('org.springframework.boot:spring-boot-starter-test')
+}
+```
+
+### Description
+By using this app, U can doing:
+1- Add your Album throw the "/addAlbum" route, so that it can be saved in a database.
+2- After adding the album U will redirect to the Albums page which include all the albums in ur DB.A user should be able to see information about all the albums on the site.
+3- U can use the header for every page to deliver between pages.
+
+### Running
+You can run the app from the class: [SongrApplication.java](src/main/java/com/example/songr/SongrApplication.java) main method, then open the browser and type `http://localhost:8080/`
--- src/main/java/com/example/songr/AlbumController.java
+++ src/main/java/com/example/songr/AlbumController.java
@@ -1,27 +1,52 @@
 package com.example.songr;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class AlbumController {
+    @Autowired
+    AlbumRepository albumRepository;
+
+
     @GetMapping("/albums")
     public String getAlbums(Model m){
-        AlbumModel redTomato = new AlbumModel("Red Tomato","Waleed",13,560,"https://i.ytimg.com/vi/ZnSx-k1yk9M/maxresdefault.jpg");
-        AlbumModel singChildren = new AlbumModel("Sing Children","children",12,450,"https://www.iom.int/sites/default/files/press_release/media/pbn_02152018_UNICEF%20Call%20to%20Action2.png");
-        AlbumModel madeEverything = new AlbumModel("Allah Made Everything","Omar Regan & Islamia School",13,326,"https://islamicresourcestore.com/wp-content/uploads/2020/06/ALLAH-MADE.jpg");
+//        AlbumModel redTomato = new AlbumModel("Red Tomato","Waleed",13,560,"https://i.ytimg.com/vi/ZnSx-k1yk9M/maxresdefault.jpg");
+//        AlbumModel singChildren = new AlbumModel("Sing Children","children",12,450,"https://www.iom.int/sites/default/files/press_release/media/pbn_02152018_UNICEF%20Call%20to%20Action2.png");
+//        AlbumModel madeEverything = new AlbumModel("Allah Made Everything","Omar Regan & Islamia School",13,326,"https://islamicresourcestore.com/wp-content/uploads/2020/06/ALLAH-MADE.jpg");
+//
+//        List<AlbumModel> albums = new ArrayList<>();
+//        albums.add(redTomato);
+//        albums.add(singChildren);
+//        albums.add(madeEverything);
 
-        List<AlbumModel> albums = new ArrayList<>();
-        albums.add(redTomato);
-        albums.add(singChildren);
-        albums.add(madeEverything);
-
-        m.addAttribute("albums",albums);
+        m.addAttribute("albums",albumRepository.findAll());
         return "albumsPage.html";
     }
+
+    @GetMapping("/addAlbum")
+    public String getAddAlbumView(){
+        return "addAlbum.html";
+    }
+
+    @PostMapping("/albums")
+    public RedirectView addAlbum(@RequestParam(value = "title") String title ,
+                                 @RequestParam(value = "artist") String artist,
+                                 @RequestParam(value = "songCount") int songCount,
+                                 @RequestParam(value = "length") int length,
+                                 @RequestParam(value = "imageUrl") String imageUrl){
+        AlbumModel newAlbum = new AlbumModel(title,artist,songCount,length,imageUrl);
+        albumRepository.save(newAlbum);
+        return new RedirectView("/albums");
+    }
+
 }
